d078d54ca8efed1837a75d3f17b841059415bcf9
Cory Kane <ckane30691@gmail.com>
2018-09-09 20:01:18 -0700

    Write app.js

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./private/mongoPath").mongoURI;
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Passport
+app.use(passport.initialize());
+require("./passport")(passport);
+
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+  app.get("/", (req, res) => res.send("Hello World"));
+  app.use("/api/users", users);
+
+  app.listen(port, () => console.log(`Server is running on port ${port}`));
